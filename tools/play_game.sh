72da43b4b1653b63efb195bb5e2bfd2eb7596483
gmorgan <gmorgan@soleranetworks.com>
2014-04-16 23:11:16 -0600

    Add debug line in play game for command line args.

--- tools/play_game.sh
+++ tools/play_game.sh
@@ -3,28 +3,30 @@
 if [ "$1" == "" ]; then
 	BOT1='python sample_bots/python/HunterBot.py'
 else
+	echo "Using $1 as BOT1"
     BOT1=$1
 fi
 
 if [ "$2" == "" ]; then
 	BOT2='python sample_bots/python/LeftyBot.py'
 else
+	echo "Using $2 as BOT2"
     BOT2=$2
 fi
-
 
 if [ "$3" == "" ]; then
 	BOT3='python sample_bots/python/HunterBot.py'
 else
+	echo "Using $3 as BOT3"
     BOT3=$3
 fi
-
 
 if [ "$4" == "" ]; then
 	BOT4='python sample_bots/python/GreedyBot.py'
 else
-    BOT1=$4
+	echo "Using $4 as BOT4"
+    BOT4=$4
 fi
 
 
-./playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/maze/maze_04p_01.map "$@" "$BOT1" "$BOT2" "$BOT3" "$BOT4"
+./playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/maze/maze_04p_01.map "$BOT1" "$BOT2" "$BOT3" "$BOT4"
