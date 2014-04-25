1ac4d550c89928ae52a0ecd17da5635af8bca5e5
gmorgan <gmorgan@soleranetworks.com>
2014-04-25 02:37:07 -0600

    Food gathering work - less broken than before.  Need collision detection work. More.

--- bots/Mission.php
+++ bots/Mission.php
@@ -2,7 +2,6 @@
 
 require_once 'State.php';
 require_once 'missions/MissionGoToPoint.php';
-require_once 'missions/MissionGoNESW.php';
 require_once 'missions/MissionPatrol.php';
 
 /**
@@ -14,9 +13,9 @@ class Mission {
 
 	const DEBUG_LEVEL_DEFAULT = AntLogger::LOG_ALL;
 
-	static $instance = 1; 	
+	static $instance = 1;
 	
-	protected $id;
+	public $id;
 
 	public $name;
 
@@ -24,20 +23,38 @@ class Mission {
 	
 	protected $debug;	
 
+	/**
+	 * The initial state of the Mission - so we can Mission.reset()
+	 *
+	 * @var State
+	 */
 	protected $initialState;
 
+	/**
+	 * The current state of the Mission - so we can Mission.end().
+	 *
+	 * @var State
+	 */
 	protected $endState;
 
 	/**
-	 * How many times a move has failed
-	 * @var type
+	 * The mission turn counter vs game turn
+	 *
+	 * @var integer
+	 */
+	protected $turn;
+
+	/**
+	 * How many times a move has failed.
+	 *
+	 * @var integer
 	 */
 	protected $stuck = 0;
 
 	/**
-	 * Try something else after a while
+	 * How many mission turns we're willing to be stuck for.
 	 *
-	 * @var type
+	 * @var integer
 	 */
 	protected $stuckThreshold = 2;
 
@@ -64,6 +81,7 @@ class Mission {
 		global $INIT_STATE;		
 		global $END_STATE;
 		global $STUCK_STATE;
+		global $MOVE_STATE;
 		
 		$this->id = (isset($args['id'])) ? $args['id'] : get_class($this) . Mission::$instance;
 		$this->name = (isset($args['name'])) ?  $args['name'] : get_class($this) . ' #' . Mission::$instance;
@@ -74,32 +92,43 @@ class Mission {
 			'logLevel' => $this->debug
 		));
 
+		$this->initialState = $INIT_STATE;
+		$this->endState = $END_STATE;
+		$this->state = $INIT_STATE;
+
 		$this->states = array(
 			'init' => $INIT_STATE,
+			'move' => $MOVE_STATE,
 			'end' => $END_STATE,
 			'stuck' => $STUCK_STATE,
 		);
-
-		$initialState = $INIT_STATE;
-		$endState = $END_STATE;
-
-		$this->state = $INIT_STATE;
 
 		$this->logger->write(sprintf("%s Initialized", $this), AntLogger::LOG_MISSION);
 		
 		Mission::$instance++;
 	}
-	
-	function reset() {
+
+	/**
+	 * Start the mission over.
+	 */
+	public function reset() {
 		$this->setState = $states[$this->initialState];
 	}
+
+	/**
+	 * End the mission.
+	 */
+	public function end() {
+		$this->setState = $states[$this->initialState];
+	}
+
 
 	/**
 	 * Return the current Mission state.
 	 * 
 	 * @return State
 	 */
-	function getState() {
+	public function getState() {
 		return $this->state;
 	}
 
@@ -109,7 +138,7 @@ class Mission {
 	 * @param State|string $state State or state id.
 	 * @return boolean Return true on success, false otherwise.
 	 */
-	function setState($state) {
+	public function setState($state) {
 		if (is_string($state)) {
 			if (isset($this->states[$state])) {
 				$newState = $this->states[$state];
@@ -130,28 +159,50 @@ class Mission {
 	}
 
 	/**
-	 * doTurn 
+	 * Do a turn for an ant. 
 	 *
-	 * Move to a new square, do an action.
+	 * Move to a new square, do an action.  Called called by bot/game loop.
 	 * 
 	 * @param Ant $ant
 	 * @param object $game Game data
 	 * @return mixed
 	 */
-	function doTurn(Ant $ant, $game = array()) {
+	public function doTurn(Ant $ant, $game = array()) {
 		$result = null;
 
+		if ($this->state->id === $this->endState->id) {
+			$this->logger->write($ant->name . " - is in it's end state.  Skipping turn ", AntLogger::LOG_GAME_FLOW | AntLogger::LOG_MISSION);
+			return true;
+		}
+
+		// do the action for the turn
+		if ($this->state->action) {
+			if (is_callable($this->state->action) || (isset($this->state->action[0]) && is_callable($this->state->action[0]))) {
+				$this->logger->write($ant->name . ' - Firing ant action "' . $this->state->actionName . '"', AntLogger::LOG_GAME_FLOW | AntLogger::LOG_MISSION);
+				if (is_callable($this->state->action)) {
+					$result = call_user_func_array($this->state->action, array($ant, $this, $game));
+				} else {
+					$args = array($ant, $this, $game);
+					if (isset($this->state->action[1]) && is_array($this->state->action[1])) {
+						array_push($args, $this->state->action[1]);
+					}
+					$result = call_user_func_array($this->state->action[0], $args);
+				}
+			} else {
+				$this->logger->write($ant->name . ' action is not callable(' . $this->state->actionName . ')', AntLogger::LOG_ERROR);
+			}
+		}
 
 		// check for events that might trigger a state change
 		foreach ($this->state->events as $evt) {
 			if (is_callable($evt['test']) || (isset($evt['test'][0]) && is_callable($evt['test'][0]))) {
 				$nextState = false;
 				if (is_callable($evt['test'])) {
-					if ($evt['test']($ant, $game)) {
+					if ($evt['test']($ant, $this, $game)) {
 						$nextState = true;
 					}
 				} else {
-					$args = array($ant, $game);
+					$args = array($ant, $this, $game);
 					if (isset($evt['test'][1]) && is_array($evt['test'][1])) {
 						array_push($args, $evt['test'][1]);
 					}
@@ -169,49 +220,30 @@ class Mission {
 			}
 		}
 
-		// do the action for the turn
-		if ($this->state->action) {
-			if (is_callable($this->state->action) || (isset($this->state->action[0]) && is_callable($this->state->action[0]))) {
-				$this->logger->write($ant->name . ' - Firing ant action "' . $this->state->actionName . '"', AntLogger::LOG_GAME_FLOW | AntLogger::LOG_MISSION);
-				if (is_callable($this->state->action)) {
-					$result = call_user_func_array($this->state->action, array($ant, $game));
-				} else {
-					$args = array($ant, $game);
-					if (isset($this->state->action[1]) && is_array($this->state->action[1])) {
-						array_push($args, $this->state->action[1]);
-					}
-					$result = call_user_func_array($this->state->action[0], $args);
-				}
-			} else {
-				$this->logger->write($ant->name . ' action is not callable(' . $this->state->actionName . ')', AntLogger::LOG_ERROR);
-			}
-		}		
-		
-		
 		return $result;
 	}	
 
 	/**
-	 * Move along a set of points. The patrol move path will be set in the transition event
+	 * The default 'move' method.
 	 *
 	 * @param Ant $ant This ant.
 	 * @param Ants $game is the Ants game data.
-	 * @return string|false Returns the direction to move next turn on success, false if nowhere to go.
+	 * @return Ouputs a game direction command to STDOUT on success, returns false otherwise.
 	 */
-	function move (Ant $ant, Ants $game) {
+	public function move (Ant $ant, Mission $mission, Ants $game) {
 
-		$nextPt = $this->getNextMove($ant, $game);
+		$nextPt = $this->getNextMove($ant, $mission, $game);
+
+		if (!$nextPt) {
+			$this->logger->write($ant->name . ' getNextMove() did not return a move.', AntLogger::LOG_MISSION);
+			return false;
+		}
 
 		// direction will be an empty array if pt0 == pt1
 		$direction = $game->direction($ant->row, $ant->col, $nextPt[0], $nextPt[1]);
 
 		if ($nextPt && $direction) {
 			$direction = $game->direction($ant->row, $ant->col, $nextPt[0], $nextPt[1]);
-
-			if ($ant->firstTurn % $game->viewradius === 0) {
-				$game->terrainMap->updateView(array($nextPt[0], $nextPt[1]), Ants::LAND);
-			}
-
 			$this->logger->write(sprintf("%s moved %s to %d,%d", $ant->name, $direction[0], $nextPt[0], $nextPt[1]), AntLogger::LOG_MISSION);
 			$game->issueOrder($ant->row, $ant->col, $direction[0]);
 			$ant->pos = array($nextPt[0], $nextPt[1]);
@@ -222,7 +254,6 @@ class Mission {
 		$this->logger->write(sprintf("%s", $ant) . ' has no where to go', AntLogger::LOG_MISSION);
 
 		return false;
-
 	} //move
 
 	/**
@@ -232,29 +263,38 @@ class Mission {
 	 * @param Ants $game
 	 * @return string|boolean
 	 */
-	protected function getNextMove(Ant $ant, Ants $game) {
-		throw new Exception ('Override this function to return n|e|s|w');
-	}
+	protected function getNextMove(Ant $ant, Mission $mission, Ants $game) {
 
-	/**
-	 * printPath
-	 * 
-	 * A path is just an array of points (arrays).
-	 * 
-	 * @param array $path
-	 * @return string
-	 */
-	public function printPath($path) {
-		$str = '';
-		if ($path) {
-			foreach ($path as $p) {
-				$str .= '(' . implode(',', $p) . ')';
+		$directions = array(
+			array($ant->row - 1, $ant->col),	// n
+			array($ant->row, $ant->col + 1),	// e
+			array($ant->row + 1, $ant->col),	// s
+			array($ant->row, $ant->col - 1)		// w
+		);
+
+		for ($i = 0; $i < 4; $i++) {
+			$nextPt = $directions[$i];
+			if ($game->passable($nextPt[0], $nextPt[1])) {
+				return $directions[$i];
 			}
 		}
-		return $str;
+
+		// for some reason the path is blocked
+
+		$this->stuck++;
+
+		$this->logger->write(sprintf("%s Path (%d, %d) blocked.", $this, $nextPt[0], $nextPt[1]), AntLogger::LOG_MISSION | AntLogger::LOG_WARN);
+
+		if ($this->stuck > $this->stuckThreshold) {
+			$this->logger->write(sprintf("%s  is stuck on path point (%d, %d). Count:%d.", $ant, $nextPt[0], $nextPt[1], $this->stuck), AntLogger::LOG_MISSION | AntLogger::LOG_WARN);
+			$this->setState('end');
+		}
+
+		return false;
 	}
 	
 	/**
+	 * Stringify a Mission object
 	 * 
 	 * @return string
 	 */
